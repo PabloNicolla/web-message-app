8f88af2c0c822a4aab850577ace1336168878ec1
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-05-25 17:13:05 -0400

    add delete server & leave server

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -19,7 +19,13 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
   if (value && fileType !== "pdf") {
     return (
       <div className="relative h-20 w-20">
-        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <Image
+          fill
+          src={value}
+          alt="Upload"
+          className="rounded-full"
+          priority
+        />
         <button
           onClick={() => onChange("")}
           className="absolute right-0 top-0 rounded-full bg-rose-500 p-1 text-white shadow-sm"
