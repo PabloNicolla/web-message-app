4525d5fc8594947b54999830be48f80905d810bc
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-05-27 22:19:43 -0400

    add remove channel modal & api

--- src/components/server/server-channel.tsx
+++ src/components/server/server-channel.tsx
@@ -53,7 +53,10 @@ export const ServerChannel = ({
       {channel.name !== "general" && role !== MemberRole.GUEST && (
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="Edit">
-            <Edit className="hidden h-4 w-4 text-zinc-500 transition hover:text-zinc-600 group-hover:block dark:text-zinc-400 dark:hover:text-zinc-300" />
+            <Edit
+              onClick={() => onOpen("editChannel", { server, channel })}
+              className="hidden h-4 w-4 text-zinc-500 transition hover:text-zinc-600 group-hover:block dark:text-zinc-400 dark:hover:text-zinc-300"
+            />
           </ActionTooltip>
           <ActionTooltip label="Delete">
             <Trash
