3848a4c7e95195722cc41a7761fc1ad0459e32a7
Pablo Nicola <nicola_pablo@yahoo.com.br>
2024-05-30 21:16:03 -0400

    add initial conversation page config (#61)
    * add initial conversation page config

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
+import { UserAvatar } from "@/components/user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -19,6 +20,9 @@ export const ChatHeader = ({
       <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="mr-2 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="mr-2 h-8 w-8 md:h-8 md:w-8" />
       )}
       <p className="text-md font-semibold text-black dark:text-white">{name}</p>
     </div>
